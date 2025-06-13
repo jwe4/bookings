cbf2ea827bee3b377fe46c4139add241f83cf556
jwe4 <jeweaver@gmail.com>
2025-06-12 21:51:35 -0600

    really through lesson 130

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -45,15 +45,6 @@ func main() {
 	fmt.Println("Starting mail listener")
 	listenForMail()
 
-	//msg := models.MailData{
-	//	To:      "john@do.ca",
-	//	From:    "me@here.com",
-	//	Subject: "Some subject",
-	//	Content: "",
-	//}
-	//
-	//app.MailChan <- msg
-
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 
 	srv := &http.Server{
