5b70a5855327e60fbeab233a6549d45050b017f7
jwe4 <jeweaver@gmail.com>
2025-06-29 16:01:40 -0600

    halfway through lesson 148

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jwe4/bookings/internal/config"
 	"github.com/jwe4/bookings/internal/driver"
 	"github.com/jwe4/bookings/internal/forms"
+	"github.com/jwe4/bookings/internal/helpers"
 	"github.com/jwe4/bookings/internal/models"
 	"github.com/jwe4/bookings/internal/render"
 	"github.com/jwe4/bookings/internal/repository"
@@ -501,4 +502,28 @@ func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 
 func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-dashboard.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-new-reservations.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request) {
+
+	reservations, err := m.DB.AllReservations()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	data := make(map[string]interface{})
+	data["reservations"] = reservations
+
+	render.Template(w, r, "admin-all-reservations.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
+}
+
+func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{})
 }
