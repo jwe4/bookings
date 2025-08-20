6e5f3d52185bc435b7db3a11136dab708b4971cf
jwe4 <jeweaver@gmail.com>
2025-08-20 16:01:08 -0600

    through lesson 152

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -377,6 +377,7 @@ func (m *postgresDBRepo) UpdateReservation(u models.Reservation) error {
 
 	query := `
           update reservations set first_name = $1, last_name = $2, email = $3, phone = $4, updated_at = $5
+          where id = $6
     `
 	_, err := m.DB.ExecContext(ctx, query,
 		u.FirstName,
@@ -384,6 +385,7 @@ func (m *postgresDBRepo) UpdateReservation(u models.Reservation) error {
 		u.Email,
 		u.Phone,
 		time.Now(),
+		u.ID,
 	)
 
 	if err != nil {
